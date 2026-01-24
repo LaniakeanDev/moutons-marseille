e16c323351127a170905e35cba8d0a71efae2be4
LaniakeanDev <laniakean.dev@gmail.com>
2026-01-24 15:02:44 +0100

    header

---
+++ app/components/header/index.tsx
@@ -0,0 +1,17 @@
+
+import ImageAsset from "../asset"
+import Title from "../title"
+
+export default function Header() {
+	return (
+		<header className="grid place-items-center relative h-28">
+			<ImageAsset 
+				src="/assets/logo.png"
+				alt="logo"
+				sizes="128px"
+				containerClassName="!absolute left-4 top-4 w-20 h-20"
+			/>
+			<Title level="h1" titleClassName="text-3xl">Les Moutons Marseillais</Title>
+		</header>
+	)
+}
