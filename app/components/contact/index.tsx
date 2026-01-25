d7fe6cfd0b57390068079bb74b552ec0a863be1c
LaniakeanDev <laniakean.dev@gmail.com>
2026-01-25 14:52:45 +0100

    footer v2

--- app/components/contact/index.tsx
+++ app/components/contact/index.tsx
@@ -1,5 +1,6 @@
 import Title from "../title";
 import SVGAsset from "../svg";
+import { ExternalLink } from "../link";
 
 interface ContactProps {
 	className: string;
@@ -7,16 +8,28 @@ interface ContactProps {
 
 export default function Contact({className}: ContactProps) {
 	return (
-		<section className={`${className}`}>
-			<Title level="h2">Contact</Title>
-			<div>
-				<p>Maïté Kaczmarek</p>
-				<p>07 43 63 21 73</p>
-				<p>lesmoutonsmarseillais@gmail.com</p>
+		<section className={`w-full grid place-items-center ${className}`}>
+			<Title 
+				containerClassName="w-full grid place-items-center mb-8"
+				titleClassName="text-3xl font-semibold"
+				level="h2"
+			>
+				Contact
+			</Title>
+			<div className="flex flex-col gap-8 items-center">
+				<p className="text-xl">Maïté Kaczmarek</p>
+				<p className="text-xl">07 43 63 21 73</p>
+				<p className="text-xl">lesmoutonsmarseillais@gmail.com</p>
 				<div className="flex flex-row gap-4">
-					<SVGAsset name="facebook" className="w-10 h-10"/>
-					<SVGAsset name="instagram" className="w-10 h-10" />
-					<SVGAsset name="linkedin" className="w-10 h-10" />
+					<ExternalLink href="https://www.facebook.com/LesMoutonsMarseillais/">
+						<SVGAsset name="facebook" className="w-10 h-10"/>
+					</ExternalLink>
+					<ExternalLink href="https://www.instagram.com/lesmoutonsmarseillais/">
+						<SVGAsset name="instagram" className="w-10 h-10" />
+					</ExternalLink>
+					<ExternalLink href="https://www.linkedin.com/company/104610172/">
+						<SVGAsset name="linkedin" className="w-10 h-10" />
+					</ExternalLink>
 				</div>
 			</div>
 		</section>
