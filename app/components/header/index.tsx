6fd222b5fc1129376b1cbb5e9d578bc3754cf305
LaniakeanDev <laniakean.dev@gmail.com>
2026-02-14 10:46:19 +0100

    header update

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -4,14 +4,19 @@ import Title from "../title"
 
 export default function Header() {
 	return (
-		<header className="grid place-items-center relative h-28">
+		<header className="sm:grid sm:place-items-center relative h-28">
 			<ImageAsset 
 				src="/assets/logo.png"
 				alt="logo"
 				sizes="128px"
-				containerClassName="!absolute left-4 top-4 w-20 h-20"
+				containerClassName="!absolute left-4 top-6 xs:top-4 w-16 h-16 xs:w-20 xs:h-20"
 			/>
-			<Title level="h1" titleClassName="text-3xl">Les Moutons Marseillais</Title>
+			<Title 
+				level="h1" 
+				titleClassName="absolute top-10 right-4 xs:right-8 sm:right-0 sm:top-0 sm:relative text-xl 2xs:text-2xl sm:text-3xl"
+			>
+				Les Moutons Marseillais
+			</Title>
 		</header>
 	)
 }
